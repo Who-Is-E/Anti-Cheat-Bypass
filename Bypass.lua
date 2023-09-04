15e2edd74f776d672f1b170e74de60ef01312b79
WhoIsE <85142651+Who-Is-E@users.noreply.github.com>
2023-09-03 22:18:12 -0500

    Update Bypass.lua

--- Bypass.lua
+++ Bypass.lua
@@ -1,8 +1,7 @@
 --[[
 MADE BY: WhoIsE.#9106
 THIS WONT BYPASS ANY INSANE ANTI CHEAT JUST DOGSHIT ONES DONT DM ME SAYING "YOUR BYPASS IS ASS".
-Edit the settings accordingly; if it throws any error's or doesn't work then DM me on discord Servers you can reach me in:
-script ware, krnl, HID systems
+Edit the settings accordingly;
 ]]
 
 -- THIS SCRIPT IS UNDERGOING A RE-Write
